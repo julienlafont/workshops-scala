d2f01bf6c5f1fa947d55588fa880806c9237d8ef
Julien Lafont <yotsumi.fx@gmail.com>
2016-05-25 13:12:00 +0200

    solutions for step3

--- workshop1-dna/src/main/scala/DnaTools.scala
+++ workshop1-dna/src/main/scala/DnaTools.scala
@@ -40,13 +40,16 @@ object DnaTools {
   /**
     * Check if the `subsequence` is contained in the main DNA sequence.
     */
-  def contains(dna: DNA, subsequence: DNA): Boolean = ???
+  def contains(dna: DNA, subsequence: DNA): Boolean = dna.containsSlice(subsequence)
 
 
   /**
     * Insert the `subsequence` at the `index` position of the DNA sequence (0-indexed)
     */
-  def insertSubsequence(dna: DNA, subsequence: DNA, index: Int): DNA = ???
+  def insertSubsequence(dna: DNA, subsequence: DNA, index: Int): DNA = {
+    val (left, right) = dna.splitAt(index)
+    left ++ subsequence ++ right
+  }
 
   /**
     * Process the Hamming distance of two DNA sequences.
@@ -65,7 +68,12 @@ object DnaTools {
     *
     * @return the hamming distance of dna1 and dna2
     */
-  def hammingDistance(dna1: DNA, dna2: DNA): Long = ???
+
+  def hammingDistance(dna1: DNA, dna2: DNA): Long = {
+    (dna1 zip dna2)
+      .count { case (l, r) => l != r }
+  }
+
 
   /**
     * Search the differences between two DNA sequences.
@@ -73,7 +81,14 @@ object DnaTools {
     * Sames rules as the Hamming distance
     * @return The indices (0 based) of the differences between the two sequences
     */
-  def basesDifferences(dna1: DNA, dna2: DNA): Seq[Int] = ???
+  def basesDifferences(dna1: DNA, dna2: DNA): Seq[Int] = {
+    (dna1 zip dna2)
+      .zipWithIndex
+      .flatMap { case ((l, r), i) =>
+        if (l != r) Some(i)
+        else None
+      }
+  }
 
   private val translationTableSource = """
      |FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG
--- workshop1-dna/src/test/scala/DnaToolsSpec.scala
+++ workshop1-dna/src/test/scala/DnaToolsSpec.scala
@@ -39,6 +39,34 @@ class DnaToolsSpec extends UnitTest {
       DnaTools.countBases(Seq(A, T, C, G)) shouldEqual Map(A -> 1, T -> 1, C -> 1, G -> 1)
       DnaTools.countBases(Seq(A, A, G, C, T, T, G, A)) shouldEqual Map(A -> 3, T -> 2, C -> 1, G -> 2)
     }
+
+    "Check if the `subsequence` is contained in the main DNA sequence" in {
+      DnaTools.contains(Seq(), Seq()) shouldEqual true
+      DnaTools.contains(Seq(), Seq(A)) shouldEqual false
+      DnaTools.contains(Seq(A, A, G, C, T, T, G, A), Seq(T, T, G)) shouldEqual true
+      DnaTools.contains(Seq(A, A, G, C, T, T, G, A), Seq(A, T, G)) shouldEqual false
+    }
+
+    "Insert a subsequence in a DNA sequence" in {
+      DnaTools.insertSubsequence(Seq(), Seq(), 0) shouldEqual Seq()
+      DnaTools.insertSubsequence(Seq(A, G), Seq(T, C), 1) shouldEqual Seq(A, T, C, G)
+    }
+
+    "Count the differences in two DNA sequences" in {
+      DnaTools.hammingDistance(Seq(A, T, C, G), Seq(A, T, C, G)) shouldEqual 0
+      DnaTools.hammingDistance(Seq(A, T, C, G), Seq(A, T)) shouldEqual 0
+      DnaTools.hammingDistance(Seq(A, T, C, G), Seq()) shouldEqual 0
+      DnaTools.hammingDistance(Seq(A, T, C, G), Seq(G, C, T, A)) shouldEqual 4
+      DnaTools.hammingDistance(Seq(A, T, C, G), Seq(A, C, C, A)) shouldEqual 2
+    }
+
+    "Return the positions of the differences between two DNA sequences" in {
+      DnaTools.basesDifferences(Seq(A, T, C, G), Seq(A, T, C, G)) shouldEqual Seq()
+      DnaTools.basesDifferences(Seq(A, T, C, G), Seq(A, T)) shouldEqual Seq()
+      DnaTools.basesDifferences(Seq(A, T, C, G), Seq()) shouldEqual Seq()
+      DnaTools.basesDifferences(Seq(A, T, C, G), Seq(G, C, T, A)) shouldEqual Seq(0, 1, 2, 3)
+      DnaTools.basesDifferences(Seq(A, T, C, G), Seq(A, C, C, A)) shouldEqual Seq(1, 3)
+    }
   }
 
 }
